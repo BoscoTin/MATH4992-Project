b2f73f9884400fb64440af7ccc1919ba92a0949a
BoscoTin <tintin111198@gmail.com>
2019-11-23 14:06:49 +0800

    Retriever option pagerank

--- src/Retriever.py
+++ src/Retriever.py
@@ -7,39 +7,53 @@ import time
 from nltk.stem import PorterStemmer
 
 db = DBManager.instance('wordcount')
+prdb = DBManager.instance('PRMatrix')
 
 # function to rank by search option
 def rank(option, keywords):
-    all = db.getAll()
+    if option != 'pr':
+        all = db.getAll()
 
-    ranker = Ranker(keywords)
-    docs_scores = []
+        ranker = Ranker(keywords)
+        docs_scores = []
 
-    otime = time.time()
+        otime = time.time()
 
-    for instance in all:
-        score = dict()
+        for instance in all:
+            score = dict()
 
-        wordlist = instance['words']
+            wordlist = instance['words']
 
-        score['url'] = instance['url']
+            score['url'] = instance['url']
 
-        # TODO: change to switch option
-        if option == 'cos':
-            score[option] = ranker.cosineSimilarity(wordlist, instance[option])
-        elif option == 'jac':
-            score[option] = ranker.jaccardSimilarity(wordlist, instance[option])
-        elif option == 'vae':
-            score[option] = ranker.variationalAutoEncoder(wordlist, instance[option])
-        else:
-            break
+            # TODO: change to switch option
+            if option == 'cos':
+                score[option] = ranker.cosineSimilarity(wordlist, instance[option])
+            elif option == 'jac':
+                score[option] = ranker.jaccardSimilarity(wordlist, instance[option])
+            elif option == 'vae':
+                score[option] = ranker.variationalAutoEncoder(wordlist, instance[option])
+            else:
+                break
 
-        docs_scores.append(score)
+            docs_scores.append(score)
 
-    sorted_scores = sorted(docs_scores, key=lambda i:i[option], reverse=True)
+        sorted_scores = sorted(docs_scores, key=lambda i:i[option], reverse=True)
 
-    return (sorted_scores, time.time() - otime)
+        return (sorted_scores, time.time() - otime)
 
+    else:
+        all = prdb.getAll()
+        docs_scores = []
+
+        for instance in all:
+            score = dict()
+            score['url'] = instance['url']
+            score[option] = instance['score']
+
+            docs_scores.append(score)
+
+        return (sorted(docs_scores, key=lambda i:i[option], reverse=True), 0.0)
 
 def custom_print(option, scores):
     print "Search results: (score, url)"
@@ -56,7 +70,7 @@ def terminate():
     print "     cos: cosine similarity measure"
     print "     jac: jaccard similarity measure"
     print "     vae: variational auto encoder measure"
-
+    print "     pr: page rank measure"
 
 def main():
     argv = sys.argv
@@ -64,7 +78,7 @@ def main():
     if "-option" in argv:
         index = argv.index("-option")
         option = argv[index + 1]
-        if option != 'cos' and option != 'jac' and option != 'vae':
+        if option != 'cos' and option != 'jac' and option != 'vae' and option != 'pr':
             terminate()
     else:
         terminate()
